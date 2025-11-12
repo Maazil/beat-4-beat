7174f81d6dac37e651d1435415cda5b43d47adf1
maazil <matthew.ling98@gmail.com>
2025-11-12 16:08:17 +0100

    delete models and change dashboard background

--- src/model/Category.ts
+++
@@ -1,7 +0,0 @@
-import { SongItem } from "./SongItem";
-
-export interface Category {
-  id: string;             // Random or auto-generated
-  name: string;           // e.g. "Pop", "Rock", "Norske Hits"
-  items: SongItem[];      // List of songs in this category
-}
